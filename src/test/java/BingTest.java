c62e7ed1b1e1b3aa72ec89a73a69463124cbd3b2
giedrius450 <147931834+giedrius450@users.noreply.github.com>
2025-11-28 20:43:11 +0200

    Update BingTest.java

--- src/test/java/BingTest.java
+++ src/test/java/BingTest.java
@@ -16,27 +16,38 @@ import java.text.SimpleDateFormat;
 import java.util.TimeZone;
 
 public class BingTest {
+    // Šis metodas paleidžiamas prieš kiekvieną testą @Test anotaciją.
+    // Jis atidaro naršyklę ir nueina į Bing pagrindinį puslapį.
     @BeforeMethod
     public void setUp() {
         Bing.setUp(Bing.URL);
     }
+    // Pagrindinis testas įveda žodį į Bing paiešką.
     @Test
     public void searchByKeywordTest(){
         Bing.searchByKeyword("Baranauskas");
     }
+    // Šis metodas vykdomas po kiekvieno testo.
+    // Čia tikrinamas testas pavyko ar nepavyko.
     @AfterMethod
     public  void close(ITestResult result) throws IOException{
+        // Jei testas sėkmingas, tiesiog uždarome naršyklę.
         if(result.isSuccess()) {
             Bing.closeBrowser();
         }else{
+            // Jei testas nepavyko, darome ekrano kopiją.
+            // Sukuriame laiko formatą ekrano kopijos pavadinimui.
             SimpleDateFormat time = new SimpleDateFormat("MM_dd_HH_mm_ss");
             time.setTimeZone(TimeZone.getTimeZone("Europe/Vilnius"));
+            // Fiksuojame laiką milisekundėmis.
             long failTime = System.currentTimeMillis();
             String failedAt = time.format(failTime);
             System.out.println(failedAt);
-
+            // Gausime testo metodo test case pavadinimą.
             String tcName = result.getName();
+            // Darome ekrano kopiją naudodami aktyvią naršyklę.
             File srcshotFile =((TakesScreenshot) Bing.getBrowser()).getScreenshotAs(OutputType.FILE);
+             // Išsaugome ekrano kopiją į katalogą /screenshots/failedc/.
             FileUtils.copyFile(srcshotFile, new File("screenshots/failedc/" + tcName + " _ " + failedAt + ".png"));
 
         }
